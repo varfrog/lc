546abdeb2996ebca4c43a5bc77c437c2d60c2e71
varfrog <x47w75@gmail.com>
2022-04-10 20:40:02 +0300

    Add applyArrayMask

--- besttravel-codewars/kata/kata.go
+++ besttravel-codewars/kata/kata.go
@@ -10,5 +10,27 @@ Mary and John?
 */
 
 func ChooseBestSum(t, k int, ls []int) int {
+	if len(ls) < k {
+		return -1
+	}
 	return 0
+}
+
+// applyArrayMask works like a bit mask. It returns a copy of ls with each nth element zeroes out if the nth element in
+// mask is zero. E.g:
+// ls = 	[1 2 3 4 5]
+// mask = 	[0 1 0 0 1]
+// result = [0 2 0 0 5]
+func applyArrayMask(ls []int, mask []int8) []int {
+	lsLen := len(ls)
+	result := make([]int, lsLen)
+	for i := 0; i < lsLen; i++ {
+		if mask[i] != 0 {
+			result[i] = ls[i]
+		} else {
+			result[i] = 0
+		}
+	}
+
+	return result
 }
--- besttravel-codewars/kata/kata_suite_test.go
+++ besttravel-codewars/kata/kata_suite_test.go
@@ -1,7 +1,6 @@
-package kata_test
+package kata
 
 import (
-	"lc/besttravel-codewars/kata"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -13,29 +12,49 @@ func TestKata(t *testing.T) {
 	RunSpecs(t, "Kata Suite")
 }
 
-func doTest(t, k int, ls []int, exp int) {
-	Expect(kata.ChooseBestSum(t, k, ls)).To(Equal(exp))
+func doChooseBestSumTest(t, k int, ls []int, exp int) {
+	Expect(ChooseBestSum(t, k, ls)).To(Equal(exp))
 }
 
 var _ = Describe("Example test cases from CodeWars", func() {
 	var ls []int
 	When("Case 1", func() {
 		It("Returns the greatest distance possible", func() {
-			doTest(163, 3, []int{50, 55, 56, 57, 58}, 163)
+			doChooseBestSumTest(163, 3, []int{50, 55, 56, 57, 58}, 163)
 		})
 	})
 	When("Case 2", func() {
 		It("Returns -1 - not enough towns", func() {
-			doTest(163, 3, []int{50}, -1)
+			doChooseBestSumTest(163, 3, []int{50}, -1)
 		})
 	})
 	When("Case 3", func() {
 		ls = []int{91, 74, 73, 85, 73, 81, 87}
 		It("Returns the greatest distance possible", func() {
-			doTest(230, 3, ls, 228)
-			doTest(331, 2, ls, 178)
-			doTest(331, 4, ls, 331)
-			doTest(331, 5, ls, -1)
+			doChooseBestSumTest(230, 3, ls, 228)
+			doChooseBestSumTest(331, 2, ls, 178)
+			doChooseBestSumTest(331, 4, ls, 331)
+			doChooseBestSumTest(331, 5, ls, -1)
+		})
+	})
+})
+
+var _ = Describe("Array mask", func() {
+	var list []int
+	var mask []int8
+	var expe []int
+	When("Case 1", func() {
+		list = []int{1, 2, 3, 4, 5}
+		mask = []int8{0, 1, 0, 0, 1}
+		expe = []int{0, 2, 0, 0, 5}
+		listAddr := &list
+		It("Masks proper values", func() {
+			Expect(applyArrayMask(list, mask)).To(Equal(expe))
+		})
+		It("Does not modify the given list", func() {
+			result := applyArrayMask(list, mask)
+			Expect(list).To(Equal([]int{1, 2, 3, 4, 5}))
+			Expect(listAddr).NotTo(Equal(result))
 		})
 	})
 })
