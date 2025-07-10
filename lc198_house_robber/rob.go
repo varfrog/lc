6a6361dbe3cda314ae07c94f57aeb4de7415ea22
Arnoldas Grigutis <x47w75@gmail.com>
2025-07-11 01:35:08 +0300

    Add 198

---
+++ lc198_house_robber/rob.go
@@ -0,0 +1,34 @@
+package lc198v1
+
+func rob(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return nums[0]
+	}
+	if n == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	// dp[a] = max money arriving to rob at house index a. Can skip house index a.
+	dp := make([]int, n)
+
+	// Base cases
+	dp[n-1] = nums[n-1]
+	dp[n-2] = max(nums[n-2], nums[n-1])
+	dp[n-3] = max(nums[n-3]+nums[n-1], nums[n-2])
+
+	// Build DP
+	for i := n - 4; i >= 0; i-- {
+		dp[i] = max(nums[i]+dp[i+2], dp[i+1])
+	}
+
+	// Get the max
+	runningMax := dp[0]
+	for i := 1; i < len(dp); i++ {
+		if dp[i] > runningMax {
+			runningMax = dp[i]
+		}
+	}
+
+	return runningMax
+}
